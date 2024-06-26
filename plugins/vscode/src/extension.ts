09c07f136c642ccbf4d3fa407c16242b733b0564
motoki317 <motoki317@gmail.com>
2024-06-26 16:50:05 +0900

    Setup release actions

--- plugins/vscode/src/extension.ts
+++ plugins/vscode/src/extension.ts
@@ -4,12 +4,25 @@ import {
 	LanguageClientOptions,
 	ServerOptions
 } from 'vscode-languageclient/node';
+import * as fs from 'node:fs';
 
 let client: LanguageClient;
 
+const getBinaryPath = (context: ExtensionContext): string => {
+	const linux = context.asAbsolutePath('./iccheck')
+	if (fs.existsSync(linux)) {
+		return linux
+	}
+	const windows = context.asAbsolutePath('./iccheck.exe')
+	if (fs.existsSync(windows)) {
+		return windows
+	}
+	throw new Error('ICCheck binary not found, perhaps an extension release misconfiguration? Please contact the extension developer.')
+}
+
 export function activate(context: ExtensionContext) {
 	const serverOptions: ServerOptions = {
-		command: context.asAbsolutePath('./iccheck'),
+		command: getBinaryPath(context),
 		args: ['lsp']
 	};
 
