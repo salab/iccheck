e2f97f014c078c709f91c978becdb3b2d9ec6f8a
motoki317 <motoki317@gmail.com>
2024-10-21 20:03:06 +0900

    perf: Use ttl for cache

--- pkg/fleccs/cache.go
+++ pkg/fleccs/cache.go
@@ -3,18 +3,22 @@ package fleccs
 import (
 	"github.com/dgraph-io/ristretto"
 	"github.com/samber/lo"
+	"time"
 	"unsafe"
 )
 
 // cache kicks in only when searching for multiple times in the same process.
 // Currently, LSP server takes advantage of this.
 var cache = lo.Must(ristretto.NewCache(&ristretto.Config[uint64, []*Candidate]{
-	NumCounters: 1e7,
-	MaxCost:     100 * 1e6, // 100MB
-	BufferItems: 64,
+	NumCounters:            1e7,
+	MaxCost:                100 * 1e6, // 100MB
+	BufferItems:            64,
+	TtlTickerDurationInSec: 60,
 }))
 
 var candidateStructBaseSize = int64(unsafe.Sizeof(Candidate{}))
+
+const cacheTTL = time.Minute
 
 func getFromCacheOrCalcCandidates(hash1, hash2 uint64, fn func() []*Candidate) []*Candidate {
 	key := (hash1 << 32) | uint64(uint32(hash2))
@@ -26,7 +30,7 @@ func getFromCacheOrCalcCandidates(hash1, hash2 uint64, fn func() []*Candidate) [
 	cost := lo.SumBy(v, func(c *Candidate) int64 {
 		return candidateStructBaseSize + int64(len(c.Filename)) + int64(len(c.Source.Filename))
 	}) + 24 // nil (8 bytes?) or slice overhead
-	cache.Set(key, v, cost)
+	cache.SetWithTTL(key, v, cost, cacheTTL)
 
 	return v
 }
