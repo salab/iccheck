dac7a457fd1ab0a08c01ef8e18ede5627bcad0f6
motoki317 <motoki317@gmail.com>
2025-01-02 19:21:54 +0900

    chores: fix test

--- main_test.go
+++ main_test.go
@@ -2,22 +2,14 @@ package main_test
 
 import (
 	"github.com/salab/iccheck/cmd"
-	"github.com/samber/lo"
 	"os"
-	"runtime/pprof"
 	"testing"
 )
 
 // For cpu profiling and algorithm optimization
 func TestSearch(t *testing.T) {
-	homedir := lo.Must(os.UserHomeDir())
-	_ = os.Mkdir(homedir+"/pprof", 0777)
-	f := lo.Must(os.Create(homedir + "/pprof/cpu.pprof"))
-	defer f.Close()
-	lo.Must0(pprof.StartCPUProfile(f))
-	defer pprof.StopCPUProfile()
-
-	err := cmd.RootCmd.RunE(cmd.RootCmd, []string{})
+	os.Args = []string{"iccheck"}
+	err := cmd.RootCmd.Execute()
 	if err != nil {
 		t.Fatal(err)
 	}
